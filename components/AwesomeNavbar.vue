9281618e6176107bd29a076fb51a179e2ce1e5a6
chloe-bowers <chlo_bo@live.com.au>
2024-04-11 16:53:09 +0200

    updated nav links

--- components/AwesomeNavbar.vue
+++ components/AwesomeNavbar.vue
@@ -2,9 +2,11 @@
   <div>
     <div class="nav-menu">
       <div class="nav-content">
-        <a href="/" class="logo">Chloe's Fun Website</a>
+        <a href="/" class="logo">Nuxt Homepage</a>
         <ul class="nav-items">
           <li><a href="/">Home</a></li>
+          <li><a href="/">Home 2</a></li>
+          <li><a href="/">Home 3</a></li>
         </ul>
       </div>
     </div>
